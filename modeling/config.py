b22080546ba171a75fe60e825a28e83bf02f0385
Tereza Iofciu <tereza@neuefische.com>
2021-03-31 12:14:51 +0200

    updated config

--- modeling/config.py
+++ modeling/config.py
@@ -4,8 +4,9 @@ import parsenvy
 logger = logging.getLogger(__name__)
 
 # mlflow
+# sometimes when saving links in text.. there is a new line .. strip removes that
 try:
-    TRACKING_URI = open(".mlflow_uri").read()
+    TRACKING_URI = open(".mlflow_uri").read().strip()
 except:
     TRACKING_URI = parsenvy.str("MLFLOW_URI")
 
